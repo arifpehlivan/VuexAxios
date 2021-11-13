af1bea340c77078c299689e686c379884a648576
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-13 16:10:04 +0300

    create PostsList.vue

--- vuex-axios/src/App.vue
+++ vuex-axios/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostsList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostsList from './components/PostsList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostsList
   }
 }
 </script>
---
+++ vuex-axios/src/components/PostsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="postsList">
+      <div v-for="post in posts" :key="post.id">
+          <h1>{{ post.title }}</h1>
+
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    computed: {
+        ...mapState('posts', ['posts'])
+    },
+    created(){
+        this.$store.dispatch('posts/loadPosts')
+    }
+}
+</script>
+
+<style>
+
+</style>
